cd60db27b5a663874457cfb94d07514446fe6d80
Anne-Zh <112821213+Anne-Zh@users.noreply.github.com>
2024-03-25 14:01:46 +0100

    ModierLicence

--- src/main/java/tp04/metier/ActionSimple.java
+++ src/main/java/tp04/metier/ActionSimple.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//*
+ */
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
